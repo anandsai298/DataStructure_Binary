29b29673d1a8b6cf7b1dfa5dc2e2a5d629986930
anandsaikumar <anandsai298@gmail.com>
2023-03-24 21:44:14 +0530

    [sai] Add : customize the string by replacing our values

---
+++ DataStructure_Binary/DataStructure_Binary/CustomizeString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure_Binary
+{
+    public class CustomizeString
+    {
+        public void Customize()
+        {
+            string Line = "Hello <<name>> we have your fullname <<fullname>> in our system with your phone number <<phno>>";
+            Line = Line.Replace("<<name>>", "ASk");
+            Line = Line.Replace("<<fullname>>", "AnandSaiKumar");
+            Line = Line.Replace("<<phno>>", "9299999769");
+            Console.WriteLine(Line);
+        }
+    }
+}
--- DataStructure_Binary/DataStructure_Binary/Program.cs
+++ DataStructure_Binary/DataStructure_Binary/Program.cs
@@ -5,14 +5,36 @@ class program
 {
     public static void Main(string[] args)
     {
-        BinarySearch search = new BinarySearch();
-        search.ReadFile(@"F:\DataStructure_Binary\DataStructure_Binary\DataStructure_Binary\TextFile1.txt");
-        InsertionSort insertionSort = new InsertionSort();
-        insertionSort.ReadFile(@"F:\DataStructure_Binary\DataStructure_Binary\DataStructure_Binary\TextFile1.txt");
-        BubbleSort bubbleSort = new BubbleSort();
-        bubbleSort.ReadFile(@"F:\DataStructure_Binary\DataStructure_Binary\DataStructure_Binary\TextFileInteger.txt");
-        Anagram anagram=new Anagram();
-        anagram.CheckAnagram();
+        bool flag = true;
+        while (flag) 
+        {
+            Console.WriteLine("1.BinarySearch\n2.InsertionSort\n3.BubbleSort\n4.Anagram\n5.CustomizeString");
+            Console.WriteLine("select option to get output");
+            int option=Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    BinarySearch search = new BinarySearch();
+                    search.ReadFile(@"F:\DataStructure_Binary\DataStructure_Binary\DataStructure_Binary\TextFile1.txt");
+                    break;
+                case 2:
+                    InsertionSort insertionSort = new InsertionSort();
+                    insertionSort.ReadFile(@"F:\DataStructure_Binary\DataStructure_Binary\DataStructure_Binary\TextFile1.txt");
+                    break;
+                case 3:
+                    BubbleSort bubbleSort = new BubbleSort();
+                    bubbleSort.ReadFile(@"F:\DataStructure_Binary\DataStructure_Binary\DataStructure_Binary\TextFileInteger.txt");
+                    break;
+                case 4:
+                    Anagram anagram = new Anagram();
+                    anagram.CheckAnagram();
+                    break;
+                case 5:
+                    CustomizeString customizeString = new CustomizeString();
+                    customizeString.Customize();
+                    break;
+            }
+        }
     }
 }
 
